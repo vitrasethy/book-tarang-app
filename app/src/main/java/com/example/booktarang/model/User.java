f62964bebb517bc9b4b21657e1608690951b5a79
Sodara Sou <sodarasou168@gmail.com>
2024-11-02 08:46:09 +0700

    update

---
+++ app/src/main/java/com/example/booktarang/model/User.java
@@ -0,0 +1,20 @@
+package com.example.booktarang.model;
+
+public class User {
+    private Integer id;
+    private String name;
+    private String email;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+}
--- app/src/main/java/com/example/booktarang/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/example/booktarang/viewmodel/ProfileViewModel.kt
@@ -1,6 +1,39 @@
 package com.example.booktarang.viewmodel
 
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.booktarang.api.ApiManager
+import com.example.booktarang.model.ApiState
+import com.example.booktarang.model.State
+import com.example.booktarang.model.User
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class ProfileViewModel: ViewModel() {
+    private val _profileState = MutableLiveData<ApiState<User>>()
+    val profileState: LiveData<ApiState<User>> get() = _profileState
+
+    fun loadProfile() {
+        val apiService = ApiManager.getApiService()
+
+        _profileState.postValue(ApiState(State.loading, null))
+        viewModelScope.launch {
+            try {
+                delay(3000)
+                val profileResponse = apiService.loadProfile()
+                if(profileResponse.isSuccess()) {
+                    _profileState.postValue(ApiState(State.success, profileResponse.data))
+                } else {
+                    _profileState.postValue(ApiState(State.error, null))
+                }
+
+            } catch (ex: Exception) {
+                Log.e("error", "Error $ex")
+                _profileState.postValue(ApiState(State.error, null))
+            }
+        }
+    }
 }
