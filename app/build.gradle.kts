02662a52f859c1affa243709d96b576d97e710ab
Sak Khakna <sthissakkhakna@gmail.com>
2025-01-24 11:08:45 +0700

    Apply build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,19 @@ android {
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("./BookTarang.jks")
+            storePassword = System.getenv("KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("KEY_ALIAS")
+            keyPassword = System.getenv("KEYSTORE_PASSWORD")
+//            storeFile = file("d:/School/MAD/BookTarang.jks")
+//            storePassword = "password"
+//            keyAlias = "BookTarang"
+//            keyPassword = "password"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
@@ -27,50 +40,66 @@ android {
             )
         }
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+    flavorDimensionList += "BookTarang"
+    productFlavors {
+        create("dev") {
+            dimension = "BookTarang"
+            applicationId = "com.example.booktarang.dev"
+            resValue("string", "app_name", "Book Tarang Test")
+            buildConfigField("String", "API_BASE_URL", "\"http://10.0.2.2:8000/api/\"")
+        }
+        create("prod") {
+            dimension = "BookTarang"
+            applicationId = "com.example.booktarang"
+            resValue("string", "app_name", "Book Tarang")
+            buildConfigField("String", "API_BASE_URL", "\"http://10.0.2.2:8000/api/\"")
+        }
+        compileOptions {
+            sourceCompatibility = JavaVersion.VERSION_1_8
+            targetCompatibility = JavaVersion.VERSION_1_8
+        }
+        kotlinOptions {
+            jvmTarget = "1.8"
+        }
+        buildFeatures {
+            viewBinding = true
+            buildConfig = true
+        }
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
+
+    dependencies {
+        implementation(libs.androidx.core.ktx)
+        implementation(libs.androidx.appcompat)
+        implementation(libs.material)
+        implementation(libs.androidx.constraintlayout)
+        implementation(libs.androidx.navigation.fragment.ktx)
+        implementation(libs.androidx.navigation.ui.ktx)
+        implementation(libs.androidx.annotation)
+        implementation(libs.androidx.lifecycle.livedata.ktx)
+        implementation(libs.androidx.lifecycle.viewmodel.ktx)
+        testImplementation(libs.junit)
+        androidTestImplementation(libs.androidx.junit)
+        androidTestImplementation(libs.androidx.espresso.core)
+
+        // Retrofit
+        implementation("com.squareup.retrofit2:retrofit:2.11.0")
+        implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+        implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
+
+        // Picasso
+        implementation("com.squareup.picasso:picasso:2.71828")
+
+        // AndroidX
+        implementation("androidx.activity:activity-ktx:1.8.2")
+        implementation("androidx.fragment:fragment-ktx:1.6.2")
+
+        // Gson
+        implementation("com.google.code.gson:gson:2.10.1")
+
+        // Security
+        implementation("androidx.security:security-crypto:1.1.0-alpha06")
+
+        // Material Design
+        implementation("com.google.android.material:material:1.11.0")
     }
-    buildFeatures {
-        viewBinding = true
-    }
-}
-
-dependencies {
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.appcompat)
-    implementation(libs.material)
-    implementation(libs.androidx.constraintlayout)
-    implementation(libs.androidx.navigation.fragment.ktx)
-    implementation(libs.androidx.navigation.ui.ktx)
-    implementation(libs.androidx.annotation)
-    implementation(libs.androidx.lifecycle.livedata.ktx)
-    implementation(libs.androidx.lifecycle.viewmodel.ktx)
-    testImplementation(libs.junit)
-    androidTestImplementation(libs.androidx.junit)
-    androidTestImplementation(libs.androidx.espresso.core)
-
-    // Retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.11.0")
-    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
-    implementation("com.squareup.okhttp3:logging-interceptor:4.12.0")
-
-    // Picasso
-    implementation("com.squareup.picasso:picasso:2.71828")
-
-    // AndroidX
-    implementation("androidx.activity:activity-ktx:1.8.2")
-    implementation("androidx.fragment:fragment-ktx:1.6.2")
-
-    // Gson
-    implementation("com.google.code.gson:gson:2.10.1")
-
-    // Security
-    implementation("androidx.security:security-crypto:1.1.0-alpha06")
-
-    // Material Design
-    implementation("com.google.android.material:material:1.11.0")
 }
